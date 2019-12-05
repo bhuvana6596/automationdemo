4a4512cad78dc4e11494745061123ffb49049e89
bhuvana6596 <58559587+bhuvana6596@users.noreply.github.com>
2019-12-05 19:43:25 +0530

    Add files via upload

---
+++ Assignment1.cs
@@ -0,0 +1,26 @@
+﻿using CSharpSeleniumFramework.POM;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSeleniumFramework.TestCase
+{
+    public class Assignment1 : TestBase
+    {
+        [Test]
+        public void TestAssignment1()
+        {
+            Home_POM home = new Home_POM();
+            home.IndustriesPage();
+            Industries_POM industries = new Industries_POM();
+            industries.BuildAutomationPage();
+            BuildAutomation_POM buildAutomationPage = new BuildAutomation_POM();
+            buildAutomationPage.NewsLetterPage();
+            buildAutomationPage.VerifyCurrentURL();
+            buildAutomationPage.TitlesOfNewsLetters();
+        }
+    }
+}
